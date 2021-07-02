a2f10d9d512ee4d7f42f054e4fc49a62b162e58a
Jens Rud Nielsen <jenrud@gmail.com>
2021-07-02 09:10:44 +0200

    refact

--- src/Raymaker.RulesEngine.Application/IUserRepository.cs
+++ src/Raymaker.RulesEngine.Application/IUserRepository.cs
@@ -7,32 +7,4 @@
         bool UpdateUser(User user);
         bool UpdateAgent(Agent agent);
     }
-
-    public class UserRepository : IUserRepository
-    {
-        public bool AddUser(User user)
-        {
-            return true;
-        }
-
-        public User GetUser(string userName)
-        {
-            return new User
-            {
-                Email = "test@test.com",
-                MembershipType = MembershipType.Basic,
-                UserName = userName
-            };
-        }
-
-        public bool UpdateAgent(Agent agent)
-        {
-            return true;
-        }
-
-        public bool UpdateUser(User user)
-        {
-            return true;
-        }
-    }
 }
---
+++ src/Raymaker.RulesEngine.Application/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace Raymaker.RulesEngine.Application
+{
+    public class UserRepository : IUserRepository
+    {
+        public bool AddUser(User user)
+        {
+            return true;
+        }
+
+        public User GetUser(string userName)
+        {
+            return new User
+            {
+                Email = "test@test.com",
+                MembershipType = MembershipType.Basic,
+                UserName = userName
+            };
+        }
+
+        public bool UpdateAgent(Agent agent)
+        {
+            return true;
+        }
+
+        public bool UpdateUser(User user)
+        {
+            return true;
+        }
+    }
+}
